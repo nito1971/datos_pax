f7d50ba06d6837d678698435a898f8fd28afae4b
nito1971 <bgonpin@gmail.com>
2024-05-13 10:40:54 +0200

    añadidos

---
+++ insertar_email_desde_csv.py
@@ -0,0 +1,45 @@
+import csv
+import pymongo
+from pymongo import MongoClient
+import re
+
+
+ruta = "/mnt/local/datos/Documentos/PASSW/BBDD_email/email.email_mongo_3.csv"
+contador = 0
+
+'''
+with open(ruta, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        if row[0] == 'indice_txt_text':
+            print(row[1])
+'''
+# Conectarse a MongoDB
+def insertar_db(_id):
+    #client = MongoClient('mongodb://10.0.0.12:27017/')
+    client = MongoClient('mongodb://localhost:27019/')
+    db = client['email']
+    collection = db['email']
+    dato = {"_id": _id }
+    try:
+        collection.insert_one(dato)
+        #print("Ok")
+    except Exception as e:
+        razon = str(e)
+        if re.search("E11000 duplicate key", razon):
+            print("Clave duplicada")
+        else:
+            print(e)
+        pass
+
+
+with open(ruta, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Salta la primera fila (encabezados)
+    
+    for row in reader:
+        #print(row[3])
+        insertar_db(row[3])
+        contador += 1
+        print(contador)
+            
--- mover_documentos.py
+++ mover_documentos.py
@@ -2,12 +2,12 @@ import pymongo
 ''' Este script mueve documentos de una base de datos mongo a otra.'''
 
 # Conexiones a las bases de datos y colecciones en servidor 1 (192.168.1.10)
-client_a = pymongo.MongoClient('mongodb://192.168.1.10:27017/email')
+client_a = pymongo.MongoClient('mongodb://10.0.0.12:27017/email')
 db_a = client_a['email']
 collection_a = db_a['email']
 
 # Conexiones a las bases de datos y colecciones en servidor 2 (192.168.1.11)
-client_b = pymongo.MongoClient('mongodb://192.168.1.11:27017/email')
+client_b = pymongo.MongoClient('mongodb://localhost:27019/email')
 db_b = client_b['email']
 collection_b = db_b['email']
 
@@ -16,18 +16,22 @@ while True:
     cursor = collection_a.find()
     try:
         for doc in cursor:
-            print(f"Documento leido: {doc['_id']},")
+            print(f"Documento leido: {doc['_id']}")
 
             # Inserta el documento en la base de datos B
             collection_b.insert_one(doc)
+            print(f"Documento insertado: {doc['_id']}")
 
             # Borra el documento de la base de datos A
             collection_a.delete_one({'_id': doc['_id']})
+            print(f"Documento borrado: {doc['_id']}")
     except Exception as e:
-        print(f"Error al leer o insertar documentos: {e}")
+        print(f"error: {e}")
+        collection_a.delete_one({'_id': doc['_id']})
+        print(f"Documento borrado: {doc['_id']}")
         pass
 
-    break  # Se puede cambiar a un bucle infinito si se desea leer documentos en bucle
+    
 
 print("Finalizado!")
 
